cffd0aa45b44a98dbb845264810a527e9ad05887
YannUFLL <yannlescoul79@free.fr>
2026-06-14 23:15:44 +0200

    feat: add argument handler for dataset path

--- separate_dataset.py
+++ separate_dataset.py
@@ -1,11 +1,16 @@
+import argparse
+
 import pandas
 
 
 if __name__ == "__main__": 
-    df = pandas.read_csv("data.csv")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset")
+    arg = parser.parse_args()
+    df = pandas.read_csv(arg.dataset)
     train_df = df.sample(frac=0.8, random_state=42)
     val_df = df.drop(train_df.index)
     train_df.to_csv("train_dataset.csv", index=False)
-    val_df.to_csv("val_df.csv", index=False)
+    val_df.to_csv("val_dataset.csv", index=False)
 
 
